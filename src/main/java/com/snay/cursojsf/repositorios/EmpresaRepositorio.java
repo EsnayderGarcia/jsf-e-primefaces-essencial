29afc469b6c7eefd0bb5aa63e2e5cb33a515601d
EsnayderGarcia <scriptk.18@gmail.com>
2022-11-05 12:11:40 -0300

    Configuração do CDI

--- src/main/java/com/snay/cursojsf/repositorios/EmpresaRepositorio.java
+++ src/main/java/com/snay/cursojsf/repositorios/EmpresaRepositorio.java
@@ -1,5 +1,50 @@
 package com.snay.cursojsf.repositorios;
 
-public class EmpresaRepositorio {
+import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import com.snay.cursojsf.models.Empresa;
+
+public class EmpresaRepositorio implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private EntityManager manager;
+	
+	public EmpresaRepositorio() {}
+
+	public EmpresaRepositorio(EntityManager manager) {
+		this.manager = manager;
+	}
+	
+	public Empresa buscarPorId(Long id) {
+		return manager.find(Empresa.class, id);
+	}
+	
+	public void remover(Long id) {
+		var entity = buscarPorId(id);
+		
+		manager.getTransaction().begin();
+		manager.remove(entity);
+		manager.getTransaction().commit();
+		manager.close();
+	}
+	
+	public List<Empresa> pesquisar(String nomeFantasia) {
+		String hql = "SELECT e FROM Empresa e WHERE e.nomeFantasia LIKE :nomeFantasia";
+		TypedQuery<Empresa> query = manager.createQuery(hql, Empresa.class);
+		List<Empresa> empresas = query
+				.setParameter("nomeFantasia", "%"+nomeFantasia+"%")
+				.getResultList();
+		
+		return empresas;
+	}
+	
+	public Empresa salvar(Empresa empresa) {
+		return manager.merge(empresa);
+	}
+	
 }
--- src/main/java/com/snay/cursojsf/repositorios/SchemaGeneration.java
+++ src/main/java/com/snay/cursojsf/repositorios/SchemaGeneration.java
@@ -1,30 +1,19 @@
 package com.snay.cursojsf.repositorios;
 
-import java.time.LocalDate;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
-import com.snay.cursojsf.enumerations.TipoEmpresa;
-import com.snay.cursojsf.models.Empresa;
 
 public class SchemaGeneration {
 	
 	public static void main(String[] args) {
 		
-		var	empresa = new Empresa(null, "Vasp", "VaspDoido", "111111111111111111", LocalDate.parse("2022-11-04"));
-		empresa.setTipoEmpresa(TipoEmpresa.MEI);
-		
-		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("curso-jsfPU");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
-		empresa = em.merge(empresa);
-		em.getTransaction().commit();
+		EmpresaRepositorio empresaRepositorio = new EmpresaRepositorio(em);
 		
-		System.out.println(em.find(Empresa.class, empresa.getId()));
+		System.out.println(empresaRepositorio.pesquisar("Ma"));
 
 	}
 	
